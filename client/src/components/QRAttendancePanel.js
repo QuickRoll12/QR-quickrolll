01473d1104d007b9083120028a807a7a10119ea4
QuickRoll12 <quickrollattendance@gmail.com>
2025-10-11 16:53:30 +0530

    Added extra function to get joined count !!

--- client/src/components/QRAttendancePanel.js
+++ client/src/components/QRAttendancePanel.js
@@ -36,7 +36,7 @@ const QRAttendancePanel = memo(({
                 totalStudents = sessionData.totalStudentsAcrossSections || 
                                (sessionData.sections ? sessionData.sections.reduce((sum, section) => sum + (section.totalStudents || 0), 0) : 0);
                 presentCount = sessionData.totalStudentsPresent || 0;
-                joinedCount = sessionData.totalStudentsJoined || 0;
+                joinedCount = sessionData.totalStudentsJoined || 0; // ULTRA-OPTIMIZED: No Redis operations during join!
             } else {
                 // For single sessions, use existing logic
                 totalStudents = sessionData.totalStudents || 0;
